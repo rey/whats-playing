5c86aa71a1273fe30dc43876e7dfc9de8e2887be
Rey Dhuny <rey@me.com>
2021-03-04 09:58:04 +0000

    First!

---
+++ whats-playing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LASTFM_API_KEY="add_your_lastfm_api_key"
+
+function go() {
+    __whats_playing
+}
+
+function __whats_playing() {
+
+    CURL_URL="http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=reydh&api_key=$LASTFM_API_KEY&format=json"
+
+    curl_response=$(curl --silent $CURL_URL)
+
+    now_playing=$(echo $curl_response | jq --raw-output '.recenttracks.track[0]."@attr".nowplaying')
+    track_artist=$(echo $curl_response | jq --raw-output '.recenttracks.track[0].artist."#text"')
+    track_name=$(echo $curl_response | jq --raw-output '.recenttracks.track[0].name')
+
+    if [[ "$now_playing" == "true" ]]; then
+        echo "📻 Playing $track_name by $track_artist"
+    else
+        echo "📻 Not playing"
+    fi
+}
+
+go
